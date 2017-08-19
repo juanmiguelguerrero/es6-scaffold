e8c74127b4a85ff6afa2ea055a27ed0b35fdb0de
juanmi <juanmiguel@existo.es>
2017-08-19 11:50:10 +0200

    Configuración test unitario básico

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+describe('A basic test', function () {
+	it('should pass when everything is okay', function () {
+		expect(true).to.be.true;
+	});
+});
+
+// Ver documentación en:
+// http://chaijs.com/guide/styles/#expect
+// http://chaijs.com/api/bdd/
